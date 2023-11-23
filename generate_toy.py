e25d6e6b92999cf829a84b87c310ee5f133c3014
erusseil <etiennerusseil63@gmail.com>
2023-11-23 22:07:02 +0100

    Updated data generation

--- generate_toy.py
+++ generate_toy.py
@@ -54,14 +54,26 @@ def create_folders(name, noises):
             os.makedirs(f"toy_data/{name}/noisy_{noise}")
 
 
-def generate_data(func, name, Xs, params, noises, npoints=25):
-    header = ["Xaxis", "yaxis"]
+def generate_data(func, name, Xs, params, noises):
+
+    if len(np.shape(Xs)) == 2:
+        header = ["Xaxis0", "yaxis"]
+    else:
+        header = []
+        for i in range(np.shape(Xs)[1]):
+            header.append(f"Xaxis{i}")
+        header.append("yaxis")
+    
     create_folders(name, noises)
 
     for idx, param in enumerate(params):
         x = Xs[idx]
-        y = func(x, *param)
-        example = np.vstack((x, y)).T
+        y = func(x.T, *param)
+
+        if len(np.shape(x.T)) == 1:
+            example = np.vstack((x.T, y)).T
+        elif len(np.shape(x.T)) == 2:
+            example = np.array([list(x.T[idx2])+[y[idx2]] for idx2 in range(len(x.T))])
 
         with open(
             f"toy_data/{name}/perfect/example{idx}.csv",
@@ -94,10 +106,10 @@ if __name__ == "__main__":
 
     # Generate polynomial data : 
     step = 0.2
-    Xs, Xs_lim = [], [[-2, 2], [-2, 2], [-2, 2], [-2, 2]]
+    Xs, Xs_lim = [], [-2, 2]
     
-    for lim in Xs_lim:
-        Xs.append(np.arange(lim[0], lim[1], step))
+    for i in range(4):
+        Xs.append(np.arange(Xs_lim[0], Xs_lim[1], step))
     
     generate_data(func_poly, "polynomial", Xs, [[2, -2, 0],[0, 2, -2],[0, 0, 2],[0, 2, 0]], noises)
 
@@ -111,11 +123,44 @@ if __name__ == "__main__":
             Xs.append(np.sort(np.concatenate([uno, uno])))
         else:
             Xs.append(np.arange(lim[0], lim[1], step))
-        
-    generate_data(func_gaussian, "gaussian", Xs, [[0, 0, 2],[2, 2, 2],[2, 0, .5],[2, 0, .5]], noises)
 
-    #generate_data(func_poly, "friedman1", [[0, 2, 2], [2, 0, 2], [2, 2, 0]], noises)
-    #generate_data(func_gaussian, "friedman2", [[2, 0.5], [0.5, 2], [0, 4]], noises)
+    generate_data(func_gaussian, "gaussian", Xs, [[0, 0, 2],[2, 0, 2],[2, 0.5, .5],[2, 0.5, .5]], noises)
+
+    # Generate friedman1 data :
+    npoints = 100
+    Xs = []
+
+    #Loop through each example
+    for _ in range(4):
+        loop = [] 
+        #Loop through each X
+        for i in range(5):
+            loop.append(np.random.random_sample(npoints))
+        Xs.append(np.array(loop))
+            
+    generate_data(func_fried1, "friedman1", Xs, [[2, 2, 2, 0],
+                                                  [2, 2, 0, 2],
+                                                  [2, 0, 2, 2],
+                                                  [0, 2, 2, 2]], noises)
+    
+    # Generate friedman2 data :
+    npoints = 100
+    Xs = []
+
+    #Loop through each example
+    for _ in range(4):
+        loop = [] 
+        loop.append(np.random.uniform(low=0, high=100, size=npoints))
+        loop.append(np.random.uniform(low=40*np.pi, high=560*np.pi, size=npoints))
+        loop.append(np.random.uniform(low=0, high=1, size=npoints))
+        loop.append(np.random.uniform(low=1, high=11, size=npoints))
+        Xs.append(np.array(loop))
+            
+    generate_data(func_fried2, "friedman2", Xs, [[2, 2, 2, 0],
+                                                  [2, 2, 0, 2],
+                                                  [2, 0, 2, 2],
+                                                  [0, 2, 2, 2]], noises)
+    
 
 
 
